979cf480691c64c7c96a056d83eff2de44874d0a
HwangSlater <doorchan23@naver.com>
2024-12-01 16:03:45 +0900

    hivemqtt

--- sensor_data_parsing/src/main/java/com/sensor_data_parsing/ControlCenter.java
+++ sensor_data_parsing/src/main/java/com/sensor_data_parsing/ControlCenter.java
@@ -1,8 +1,12 @@
 package com.sensor_data_parsing;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 
 public class ControlCenter {
@@ -32,13 +36,48 @@ public class ControlCenter {
 
                 // 3. 특정 토픽을 구독하고 메시지를 수신
                 client.toAsync().subscribeWith()
-                                .topicFilter("data/#") // 'data/'로 시작하는 모든 토픽 구독
+                                .topicFilter("application/#") // 'application/'로 시작하는 모든 토픽 구독
                                 .callback(publish -> {
-                                        // 받은 메시지의 토픽과 payload 출력
-                                        System.out.println("Received message on topic " + publish.getTopic() + ": " +
-                                                        new String(publish.getPayloadAsBytes(),
-                                                                        StandardCharsets.UTF_8));
+                                        String message = new String(publish.getPayloadAsBytes(),
+                                                        StandardCharsets.UTF_8);
+
+                                        ObjectMapper objectMapper = new ObjectMapper();
+                                        try {
+                                                JsonNode rootNode = objectMapper.readTree(message);
+                                                // deviceName 추출
+                                                String deviceName = rootNode.path("deviceInfo").path("deviceName")
+                                                                .asText();
+                                                // spot 추출
+                                                String spotName = rootNode.path("deviceInfo").path("tags").path("name")
+                                                                .asText();
+                                                System.out.println("deviceName: " + deviceName);
+                                                System.out.println("spot: " + spotName);
+
+                                                // data를 추출하기위해 object 추출
+                                                JsonNode objectNode = rootNode.path("object");
+                                                // dataName: dataValue를 맵에 저장
+                                                Map<String, Object> dataMap = objectMapper
+                                                                .readValue(objectNode.toString(), Map.class);
+
+                                                // data 출력
+                                                System.out.println("----------data----------");
+                                                for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
+                                                        System.out.println(entry.getKey() + ": " + entry.getValue());
+                                                }
+                                                System.out.println();
+                                        } catch (IOException e) {
+                                        }
+
                                 })
                                 .send();
+
+                // 4. 메시지 발행
+                // String topic = "test/topic";
+                // String message = "temperature: 20";
+                // client.toAsync().publishWith()
+                // .topic(topic)
+                // .payload(message.getBytes(StandardCharsets.UTF_8))
+                // .send();
+                // System.out.println("Message published to topic " + topic + ": " + message);
         }
 }
