4ca16f6ec828ad00688374eb830feb31a88eb295
HwangSlater <doorchan23@naver.com>
2024-12-13 14:03:13 +0900

    sad

--- sensor_data_parsing/src/main/java/com/sensor_data_parsing/SimpleMasterTCP.java
+++ sensor_data_parsing/src/main/java/com/sensor_data_parsing/SimpleMasterTCP.java
@@ -1,6 +1,8 @@
 package com.sensor_data_parsing;
 
 import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
@@ -10,43 +12,56 @@ import com.intelligt.modbus.jlibmodbus.exception.ModbusNumberException;
 import com.intelligt.modbus.jlibmodbus.exception.ModbusProtocolException;
 import com.intelligt.modbus.jlibmodbus.master.ModbusMaster;
 import com.intelligt.modbus.jlibmodbus.master.ModbusMasterFactory;
-import com.intelligt.modbus.jlibmodbus.msg.request.ReadHoldingRegistersRequest;
-import com.intelligt.modbus.jlibmodbus.msg.response.ReadHoldingRegistersResponse;
+import com.intelligt.modbus.jlibmodbus.msg.request.ReadInputRegistersRequest;
+import com.intelligt.modbus.jlibmodbus.msg.response.ReadInputRegistersResponse;
 import com.intelligt.modbus.jlibmodbus.tcp.TcpParameters;
 
-/*
- * Copyright (C) 2016 "Invertor" Factory", JSC
- * All rights reserved
- *
- * This file is part of JLibModbus.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
- * or promote products derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Authors: Vladislav Y. Kochedykov, software engineer.
- * email: vladislav.kochedykov@gmail.com
- */
 public class SimpleMasterTCP {
+    // 메시지를 보낼 브로커
+    private static final String newMqttHost = "localhost"; // MQTT 브로커 주소
+    private static final String newMqttUsername = ""; // MQTT 사용자 이름
+    private static final String newMqttPassword = ""; // MQTT 비밀번호
 
-    static public void main(String[] args) {
+    private static final Map<Integer, String> addressMap = new HashMap<>();
+    static {
+        addressMap.put(0, "operation Heartbit");
+        addressMap.put(1, "temperature");
+        addressMap.put(2, "frequency");
+        addressMap.put(3, "program version");
+        addressMap.put(4, "present CO2 use(month)");
+        addressMap.put(6, "operation Heartbit");
+        addressMap.put(7, "temperature 1");
+        addressMap.put(8, "frequency");
+        addressMap.put(9, "program version");
+        addressMap.put(10, "present CO2 use(month)");
+        addressMap.put(12, "V123(LN) average");
+        addressMap.put(13, "V123(LL) average");
+        addressMap.put(14, "V123(LN) unbalance");
+        addressMap.put(15, "V123(LL) unbalance");
+        addressMap.put(16, "V1");
+        addressMap.put(17, "V12");
+        addressMap.put(18, "V1 unbalance");
+        addressMap.put(19, "V12 unbalance");
+        addressMap.put(20, "V2");
+        addressMap.put(21, "V23");
+        addressMap.put(22, "V2 unbalance");
+        addressMap.put(23, "V23 unbalance");
+        addressMap.put(24, "V3");
+        addressMap.put(25, "V31");
+        addressMap.put(26, "V3 unbalance");
+        addressMap.put(27, "V31 unbalance");
+        addressMap.put(28, "V1 THD");
+        addressMap.put(29, "V2 THD");
+        addressMap.put(30, "V3 THD");
+    }
+
+    public static void main(String[] args) {
+        final Mqtt5Client newMqttClient = Mqtt5Client.builder()
+                .identifier("controlcenter-5678") // 클라이언트 식별자
+                .serverHost(newMqttHost)
+                .automaticReconnectWithDefaultConfig() // 자동 재연결
+                .serverPort(8888)
+                .build();
 
         Modbus.log().addHandler(new Handler() {
             @Override
@@ -80,8 +95,8 @@ public class SimpleMasterTCP {
             Modbus.setAutoIncrementTransactionId(true);
 
             int slaveId = 1;
-            int offset = 0;
-            int quantity = 10;
+            int offset = 100;
+            int quantity = 32;
 
             try {
                 // since 1.2.8
@@ -89,31 +104,46 @@ public class SimpleMasterTCP {
                     m.connect();
                 }
 
-                // at next string we receive ten registers from a slave with id of 1 at offset
-                // of 0.
-                int[] registerValues = m.readHoldingRegisters(slaveId, offset, quantity);
-
-                for (int value : registerValues) {
-                    System.out.println("Address: " + offset++ + ", Value: " + value);
-                }
-                // also since 1.2.8.4 you can create your own request and process it with the
-                // master
-                offset = 0;
-                ReadHoldingRegistersRequest request = new ReadHoldingRegistersRequest();
-                request.setServerAddress(1);
+                ReadInputRegistersRequest request = new ReadInputRegistersRequest();
+                request.setServerAddress(slaveId);
                 request.setStartAddress(offset);
-                request.setTransactionId(0);
-                ReadHoldingRegistersResponse response = (ReadHoldingRegistersResponse) m.processRequest(request);
-                // you can get either int[] containing register values or byte[] containing raw
-                // bytes.
-                for (int value : response.getRegisters()) {
-                    System.out.println("Address: " + offset++ + ", Value: " + value);
+                request.setQuantity(quantity);
+                request.setTransactionId(1);
+
+                ReadInputRegistersResponse response = (ReadInputRegistersResponse) m.processRequest(request);
+
+                int addressIncrement = 0;
+                // 레지스터 값을 읽고 처리
+                for (int i = 0; i < response.getHoldingRegisters().getQuantity(); i++) {
+                    int registerValue = response.getHoldingRegisters().get(i);
+                    if (i == 5 || i == 10) {
+                        addressIncrement++;
+                    }
+                    int address = offset + addressIncrement; // 주소는 i에 맞게 증가
+
+                    if (address - offset == 12 ||
+                            address - offset == 13 ||
+                            address - offset == 16 ||
+                            address - offset == 17 ||
+                            address - offset == 20 ||
+                            address - offset == 21 ||
+                            address - offset == 24 ||
+                            address - offset == 25) {
+
+                        // 32비트 값 처리 (두 개의 16비트 레지스터 결합)
+                        if (i < response.getHoldingRegisters().getQuantity() - 1) {
+                            registerValue = (response.getHoldingRegisters().get(i) << 16)
+                                    | response.getHoldingRegisters().get(i + 1);
+                            System.out.println("Address: " + address + ", Value: " + registerValue);
+                            i++; // 두 개의 레지스터를 처리했으므로 i를 1 증가시킴
+                        }
+                    } else {
+                        // 16비트 값 처리
+                        System.out.println("Address: " + address + ", Value: " + registerValue);
+                    }
+                    addressIncrement++;
                 }
-            } catch (ModbusProtocolException e) {
-                e.printStackTrace();
-            } catch (ModbusNumberException e) {
-                e.printStackTrace();
-            } catch (ModbusIOException e) {
+            } catch (ModbusProtocolException | ModbusNumberException | ModbusIOException e) {
                 e.printStackTrace();
             } finally {
                 try {
